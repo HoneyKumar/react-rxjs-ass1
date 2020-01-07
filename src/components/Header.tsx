0129a7e7a656d9497caadda0987fed61c8150f50
Honey Kumar <honey.kumar@incedoinc.com>
2020-01-07 17:03:19 +0530

    flexbox design implementation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,20 +2,28 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 function Header(){
     return(
-        <div className="ui menu">
-            <div className="header item">
-                Incedo
+        <React.Fragment>
+        {/* Header  Starts*/}
+            <div className="header">
+                <h1>Assignment One</h1>
+                <p>With a <b>flexible</b> layout.</p>
             </div>
-            <Link className="item" to="/home">
+        {/* Header  Ends*/}
+        {/* Navbar Starts*/}
+        <div className="navbar">
+            
+            <Link className="nav_menu" to="/home">
                 Home
             </Link>
-            <Link className="item" to="/about-us">
+            <Link className="nav_menu" to="/about-us">
                 About Us
             </Link>
-            <Link className="item" to="/login">
+            <Link className="nav_menu" to="/login">
                 Login
             </Link>
         </div>
+        {/* Navbar Ends */}
+        </React.Fragment>
     );
 }
 
